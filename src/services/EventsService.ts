605c19cdf523eb4f878dedce1862f52972bce616
razker <razker@gmail.com>
2022-11-13 18:54:36 +0200

    wip

--- src/services/EventsService.ts
+++ src/services/EventsService.ts
@@ -2,11 +2,13 @@ import axios from "axios";
 import { EventType } from "../global/types";
 import { eventTypeToString } from "../global/helpers";
 
+const WAITING_SERVICE_URL = process.env.REACT_APP_WAITING_SERVICE_URL;
+
 export class EventsService {
   public async getEvents(): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
-        const response = await axios.get("/api/events");
+        const response = await axios.get(`${WAITING_SERVICE_URL}/api/events`);
         resolve(response?.data);
       } catch (error) {
         reject(error);
@@ -20,7 +22,7 @@ export class EventsService {
   ): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
-        const response = await axios.post("/api/event", {
+        const response = await axios.post(`${WAITING_SERVICE_URL}/api/event`, {
           eventType: eventTypeToString(eventType),
           eventDate,
         });
@@ -38,10 +40,13 @@ export class EventsService {
   ): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
-        const response = await axios.post(`/api/waiting-list/${eventId}`, {
-          fullName,
-          phoneNumber,
-        });
+        const response = await axios.post(
+          `${WAITING_SERVICE_URL}/api/waiting-list/${eventId}`,
+          {
+            fullName,
+            phoneNumber,
+          }
+        );
         resolve(response);
       } catch (error) {
         reject(error);
@@ -52,7 +57,9 @@ export class EventsService {
   public async getEventWaitingList(eventId: string): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
-        const response = await axios.get(`/api/waiting-list/${eventId}`);
+        const response = await axios.get(
+          `${WAITING_SERVICE_URL}/api/waiting-list/${eventId}`
+        );
         resolve(response?.data);
       } catch (error) {
         reject(error);
@@ -67,7 +74,7 @@ export class EventsService {
     return new Promise(async (resolve, reject) => {
       try {
         const response = await axios.put(
-          `/api/waiting-list/${waitingListNodeId}`,
+          `${WAITING_SERVICE_URL}/api/waiting-list/${waitingListNodeId}`,
           { isActive: false, exchangeName }
         );
         resolve(response?.data);
